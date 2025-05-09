19406a4ad24bd5d4d58faa3a728fdeae6db1cdc7
dangmooh <zvxc300@gmail.com>
2025-05-09 17:26:16 +0900

    DC-29-chore/sensor/stm32f103rb/main/디렉토리_구조/홍다윗: 디렉토리_구조_및_uart_cli
    - 디렉토리 구조 설계
    - uart 설계
    - cli 설계계

---
+++ code/stm32f103rb/sensor/main/src/ap/ap.h
@@ -0,0 +1,19 @@
+/*
+ * ap.h
+ *
+ *  Created on: May 2, 2025
+ *      Author: USER
+ */
+
+#ifndef SRC_AP_AP_H_
+#define SRC_AP_AP_H_
+
+
+#include "hw.h"
+
+
+void ap_init(void);
+void ap_main(void);
+
+
+#endif /* SRC_AP_AP_H_ */
---
+++ code/stm32f103rb/sensor/main/src/hw/hw.c
@@ -0,0 +1,23 @@
+/*
+ * hw.c
+ *
+ *  Created on: May 2, 2025
+ *      Author: DAWIT
+ */
+
+
+#include "hw.h"
+
+
+
+
+
+
+void hw_init(void)
+{
+  bsp_init();
+
+  cliInit();
+  led_init();
+  uartInit();
+}
